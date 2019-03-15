e0fd56caf00504e9c7728e90f1841587621b923d
Mike <parolinimichael@gmail.com>
2019-03-15 16:29:32 +0000

    Added base galaxy calculations

---
+++ ECSProject/Assets/RotateAround.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class RotateAround : MonoBehaviour {
+
+    public GameObject Cube;
+    public float speed;
+
+    public float xDist;
+    public float zDist;
+    public float distance;
+    private void Start()
+    {
+       
+        speed = 0.01f/((Cube.transform.position.magnitude - transform.position.magnitude)*(Time.deltaTime*0.1f));
+        
+
+
+
+        Debug.Log("xDist: " + xDist + "  "+"zDist: "+zDist);
+    }
+
+
+    private void Update()
+    {
+        transform.RotateAround(Cube.transform.position, Vector3.up, speed /** Time.deltaTime*/);
+    }
+}
---
+++ ECSProject/Assets/rotator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotator : MonoBehaviour {
+
+    public float speed;
+
+
+
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Rotate(Vector3.up, speed * Time.deltaTime);
+
+	}
+}
