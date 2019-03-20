65268137685a565cbc614650730a6196d5bc5d92
Yggahs <alessio.calore96@gmail.com>
2019-03-20 15:53:03 +0000

    stars now correctly instantiate at 5 units from eachother

--- ECSProject/Assets/PURE ECS/Scripts/Systems/GameManager.cs
+++ ECSProject/Assets/PURE ECS/Scripts/Systems/GameManager.cs
@@ -10,13 +10,14 @@ public class GameManager : MonoBehaviour {
 
     EntityManager manager;
     public GameObject GameObjectEntity;
-    public int amount;
+    public int nucleusAmount;
     public int starAmount;
-	// Use this for initialization
-	void Start () {
+    
+    // Use this for initialization
+    void Start () {
 
         manager = World.Active.GetOrCreateManager<EntityManager>();
-        AddCube(amount);
+        AddCube(nucleusAmount, starAmount);
 	}
 
 
@@ -24,48 +25,45 @@ public class GameManager : MonoBehaviour {
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            AddCube(amount);
+            AddCube(nucleusAmount, starAmount);
         }
     }
 
-    void AddCube(int amount)
+    void AddCube(int nucleusAmount, int starAmount)
     {
-        float posx;
-        float posz;
+        
         Position vectorpos;
         int NCDistance = 5;//nucleus-star distance
-        NativeArray<Entity> entities = new NativeArray<Entity>(amount, Allocator.Temp);
-        manager.Instantiate(GameObjectEntity, entities);
-        for (int i = 0; i < amount; i++)
+        NativeArray<Entity> nucleusEntities = new NativeArray<Entity>(nucleusAmount, Allocator.Temp);
+        manager.Instantiate(GameObjectEntity, nucleusEntities);
+        for (int i = 0; i < nucleusAmount; i++)
         {
-            manager.SetComponentData(entities[i], new Position { Value = new float3(UnityEngine.Random.Range(1, 10), 0, UnityEngine.Random.Range(1, 10)) });
-            vectorpos = manager.GetComponentData<Position>(entities[i]);
-            posx = vectorpos.Value.x;
-            posz = vectorpos.Value.z;
-            AddAnotherCube(starAmount, i, entities[i], NCDistance,posx,posz);
-            manager.SetComponentData(entities[i], new Speed { Value = 10f});
-            
-            
-
+            manager.SetComponentData(nucleusEntities[i], new Position { Value = new float3(UnityEngine.Random.Range(1, 100), 0, UnityEngine.Random.Range(1, 100)) });
+            vectorpos = manager.GetComponentData<Position>(nucleusEntities[i]);
+            float posx = vectorpos.Value.x;
+            float posz = vectorpos.Value.z;
+            manager.SetComponentData(nucleusEntities[i], new Speed { Value = 10f});    
+           AddAnotherCube(NCDistance, GameObjectEntity, posx + NCDistance, posz + NCDistance,nucleusAmount);  
         }
-        entities.Dispose();
+        nucleusEntities.Dispose();
     }
 
-    void AddAnotherCube(int starAmount,int i,Entity entity, int NCDistance,float posx,float posz)
+    void AddAnotherCube(int NCDistance, GameObject GO, float posx, float posz,int nucleusAmount)
     {
-        NativeArray<Entity> entities = new NativeArray<Entity>(starAmount, Allocator.Temp);
-        //manager.Instantiate(GameObjectEntity, entities,GameObjectEntity2);
-        manager.Instantiate(entity, entities);
-        //for (int i = 0; i < amount; i++)
+        NativeArray<Entity> starEntities = new NativeArray<Entity>(starAmount, Allocator.Temp);
+        manager.Instantiate(GameObjectEntity, starEntities);
+
+        //inserire ogni gameobject instanziato su una lista (NativeList<Entity>(quantita' stelle)),instaziare stelle e poi distruggere native array in modo di avere una lista contente ogni entita' stella disponibile per modifica
 
 
-        //{
-        manager.SetComponentData(entities[i], new Position {Value = new float3(posx + NCDistance + 5, 0, posz+NCDistance + 5)});
-        
-        manager.SetComponentData(entities[i], new Speed { Value = 10f });
-
-        //}
-        entities.Dispose();
+        for (int j = 0; j < starAmount; j++)
+        {
+            manager.SetComponentData(starEntities[j], new Position { Value = new float3(posx + NCDistance, 0, posz + NCDistance) });
+            //manager.SetComponentData(starEntities[j], new Rotation { Value = new Quaternion(0, 0, 90,0) });
+            manager.SetComponentData(starEntities[j], new Speed { Value = UnityEngine.Random.Range(1f, 20f) });
+            NCDistance += 5;
+        }
+        starEntities.Dispose();
     }
 
 }
