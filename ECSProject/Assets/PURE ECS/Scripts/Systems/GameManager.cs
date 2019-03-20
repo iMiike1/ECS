57e609288ce2227c97d4f46748cbaae32a47cc7d
Yggahs <alessio.calore96@gmail.com>
2019-03-20 18:46:18 +0000

    started rotation

--- ECSProject/Assets/PURE ECS/Scripts/Systems/GameManager.cs
+++ ECSProject/Assets/PURE ECS/Scripts/Systems/GameManager.cs
@@ -12,7 +12,7 @@ public class GameManager : MonoBehaviour {
     public GameObject GameObjectEntity;
     public int nucleusAmount;
     public int starAmount;
-    
+    public static float3 RotateAroundPoint(float3 position, float3 pivot, float3 axis, float delta) => math.mul(quaternion.AxisAngle(axis, delta), position - pivot) + pivot;
     // Use this for initialization
     void Start () {
 
@@ -41,29 +41,38 @@ public class GameManager : MonoBehaviour {
             manager.SetComponentData(nucleusEntities[i], new Position { Value = new float3(UnityEngine.Random.Range(1, 100), 0, UnityEngine.Random.Range(1, 100)) });
             vectorpos = manager.GetComponentData<Position>(nucleusEntities[i]);
             float posx = vectorpos.Value.x;
+            float posy = vectorpos.Value.y;
             float posz = vectorpos.Value.z;
+            Vector3 originPos = new Vector3(posx, posy, posz);
             manager.SetComponentData(nucleusEntities[i], new Speed { Value = 10f});    
-           AddAnotherCube(NCDistance, GameObjectEntity, posx + NCDistance, posz + NCDistance,nucleusAmount);  
+           AddAnotherCube(NCDistance, originPos);
+            
         }
         nucleusEntities.Dispose();
     }
 
-    void AddAnotherCube(int NCDistance, GameObject GO, float posx, float posz,int nucleusAmount)
+    void AddAnotherCube(int NCDistance, Vector3 originPos)
     {
         NativeArray<Entity> starEntities = new NativeArray<Entity>(starAmount, Allocator.Temp);
         manager.Instantiate(GameObjectEntity, starEntities);
-
         //inserire ogni gameobject instanziato su una lista (NativeList<Entity>(quantita' stelle)),instaziare stelle e poi distruggere native array in modo di avere una lista contente ogni entita' stella disponibile per modifica
-
-
         for (int j = 0; j < starAmount; j++)
         {
-            manager.SetComponentData(starEntities[j], new Position { Value = new float3(posx + NCDistance, 0, posz + NCDistance) });
-            //manager.SetComponentData(starEntities[j], new Rotation { Value = new Quaternion(0, 0, 90,0) });
+            
+            //GameObjectEntity.transform.RotateAround(originPos, Vector3.up, 10f * Time.deltaTime);
+            //manager.SetComponentData(starEntities[j], new Transform { Value = new Vector3(1,1,1) });
+            //var gesu = manager.GetComponentData<Transform>(nucleusEntities[i]);
+
+
+            manager.SetComponentData(starEntities[j], new Position { Value = new float3(originPos.x + NCDistance, 0, originPos.z + NCDistance) });
             manager.SetComponentData(starEntities[j], new Speed { Value = UnityEngine.Random.Range(1f, 20f) });
             NCDistance += 5;
+            Position anus = manager.GetComponentData<Position>(starEntities[j]);
+            
+            RotateAroundPoint(anus.Value,originPos,Vector3.up,Time.deltaTime*200);
         }
         starEntities.Dispose();
     }
+    
 
 }
--- ECSProject/Assets/PURE ECS/Scripts/Systems/MovementComponentSystem.cs
+++ ECSProject/Assets/PURE ECS/Scripts/Systems/MovementComponentSystem.cs
@@ -3,11 +3,10 @@ using Unity.Jobs;
 using Unity.Mathematics;
 using System;
 using Unity.Transforms;
+using UnityEngine;
 
 public class MovementComponentSystem : JobComponentSystem
 {
-    //public JobHandle MoveHandle { get; private set; }
-    //[ComputeJobOptimization]
     struct MovementJob : IJobProcessComponentData<Position, Rotation, Speed>
     {
 
@@ -15,11 +14,24 @@ public class MovementComponentSystem : JobComponentSystem
         //public float SValue;
         public void Execute(ref Position pos, ref Rotation rot, ref Speed speed)
         {
-
+            
             float3 PositionValue = pos.Value;
             float SValue = speed.Value;
+            
             PositionValue.y -= 1.0f * DeltaTime;
             pos.Value.y = PositionValue.y;
+            float4 RotationValue = rot.Value.value;
+
+            Quaternion rotation = new Quaternion(RotationValue.x, RotationValue.y, RotationValue.z, RotationValue.w);
+            Vector3 euler = rotation.eulerAngles;
+            euler.y += DeltaTime * 100.00f;
+
+            rotation.eulerAngles = euler;
+
+            RotationValue = new float4(rotation.x, rotation.y, rotation.z, rotation.w);
+
+            rot.Value.value = RotationValue;
+
             
             speed.Value = SValue;
         }
@@ -30,10 +42,8 @@ public class MovementComponentSystem : JobComponentSystem
     {
         MovementJob MJob = new MovementJob
         {
-            DeltaTime = UnityEngine.Time.deltaTime,
-            
-            
-    };
+            DeltaTime = Time.deltaTime,          
+        };
        
         JobHandle MoveHandle = MJob.Schedule(this, inputDeps);
 
